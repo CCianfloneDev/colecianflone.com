f1f38a0a3a7a4b3504f3220243ce5430a8d4562c
= <colecianflone123@gmail.com>
2025-06-02 21:02:36 -0500

    Created projects page

---
+++ app/routes/projects.tsx
@@ -0,0 +1,13 @@
+export default function Projects() {
+  return (
+    <section className="max-w-2xl mx-auto px-6 py-8 text-center">
+      <h1 className="text-3xl font-bold mb-4">Projects</h1>
+      <ul className="list-disc pl-8 text-left inline-block">
+        {/* Replace the following list items with your actual projects 
+        <li>Project 1: Description of your project.</li>
+        <li>Project 2: Description of another project.</li>
+        {/* Add more projects as needed */}
+      </ul>
+    </section>
+  );
+}
