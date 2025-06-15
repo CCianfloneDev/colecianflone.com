d7c23cabe8abd7899119e603d6ddbb3c24c63418
CCianfloneDev <colecianflone123@gmail.com>
2025-06-14 20:17:00 -0500

    Added more details to sitemap generation script

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -21,14 +21,19 @@ const urls = [
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls
-  .map(
-    (url) => `<url>
-      <loc>${url}</loc>
-      <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
-    </url>`
-  )
-  .join("\n")}
+${urls.map((url) => {
+  // Determine frequency based on URL pattern
+  let changefreq = "monthly";
+  if (url === `${BASE_URL}/`) changefreq = "weekly";
+  if (url.includes("/blog/")) changefreq = "monthly";
+  
+  return `<url>
+    <loc>${url}</loc>
+    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+    <changefreq>${changefreq}</changefreq>
+    <priority>${url === BASE_URL ? "1.0" : "0.8"}</priority>
+  </url>`;
+}).join("\n")}
 </urlset>
 `;
 
